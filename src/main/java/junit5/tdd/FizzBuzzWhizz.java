e840e797ccd2c3f3b442060a7aa7e01d2ac64456
YORK ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGYO5@OOCL.COM>
2020-07-23 12:40:32 +0800

    [FizzBuzzWhizz][York] feature: given 15 when play fizz buzz whizz return fizz buzz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -2,15 +2,20 @@ package junit5.tdd;
 
 public class FizzBuzzWhizz {
     String play(int number) {
+        String result = "";
         if(number % 3 == 0) {
-            return "fizz";
+            result += "fizz";
         }
         if(number % 5 == 0) {
-            return "buzz";
+            result += "buzz";
         }
         if(number % 7 == 0) {
-            return "whizz";
+            result += "whizz";
         }
-        return String.valueOf(number);
+        if(result.equals("")) {
+            return String.valueOf(number);
+        } else {
+            return result;
+        }
     }
 }
